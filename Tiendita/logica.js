b393a08c07e297e5224273de9311f4ca0fa4a452
MakiMike <josemigueltalaverauribe@hotmail.com>
2025-03-13 09:21:36 -0600

    js

---
+++ Tiendita/logica.js
@@ -0,0 +1,14 @@
+const producto = {
+    nombre: 'Pantalos Masculino',
+    precio: 200,
+    stock: 100,
+    disponible: true
+}
+
+const producto2 = {
+    nombre: 'Pantalos Femenino',    
+    precio: 150,
+    stock: 50,
+    disponible: true
+}
+
